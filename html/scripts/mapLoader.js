b160aa9dfffb5f7a09fd50aced92a551d898043b
Kai-Arne Reiter <k.reiter@gebatrans.com>
2024-09-06 09:12:46 +0200

    map2

--- html/scripts/mapLoader.js
+++ html/scripts/mapLoader.js
@@ -1,6 +1,6 @@
 export async function loadSVG() {
     try {
-        const response = await fetch('map.svg');
+        const response = await fetch('map2.svg');
         const svgContent = await response.text();
         document.getElementById('map-container').innerHTML += svgContent;
         const svgElement = document.querySelector('#map-container svg');
